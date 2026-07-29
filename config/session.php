71a77bae0e7c615d3bca9443a543c1e213fac4bd
kalsariyaarpan <kalsariyaarpan1257@gmail.com>
2026-07-29 17:20:58 +0530

    session changes

--- config/session.php
+++ config/session.php
@@ -18,7 +18,9 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'database'),
+    'driver' => env('APP_ENV', 'local') === 'production'
+        ? 'database'
+        : env('SESSION_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
